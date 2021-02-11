388f6287b06a6b420d66f27a55e4bfaaec9127f6
Trevor Tsai <trevortsai1@gmail.com>
2021-02-11 00:37:09 -0800

    Begin work on script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  const url = "https://fakestoreapi.com/products";
+  fetch(url).then(response => response.json())
+  .then(data => {
+    let dataString = JSON.stringify(data);
+    localStorage.setItem('items', dataString);
+  })
 });
